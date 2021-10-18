2103106b4cf30de9686414f843548da8a9d39eb6
IamGiel <lfagel84@gmail.com>
2021-10-18 11:24:25 -0400

    dynamic render from dummy data

--- src/app/abi-module/preview/preview.component.ts
+++ src/app/abi-module/preview/preview.component.ts
@@ -21,10 +21,54 @@ export class PreviewComponent implements OnInit {
       "chatid":345,
       "date":'09 Sep 2023',
       "module":"Pending"
+    },
+    {
+      "chatid":123,
+      "date":'09 Sep 2023',
+      "module":"Supplier Intelligence"
+    },
+    {
+      "chatid":234,
+      "date":'09 Sep 2023',
+      "module":"Supplier Intelligence"
+    },
+    {
+      "chatid":345,
+      "date":'09 Sep 2023',
+      "module":"Pending"
+    }
+  ];
+  projects = [
+    {
+      "chatid":123,
+      "date":'09 Sep 2023',
+      "name":"Charlie Jo",
+      "status":"requested"
+    },
+    {
+      "chatid":234,
+      "date":'09 Sep 2023',
+      "name":"Linh Nguyen",
+      "status":"completed"
+    },
+    {
+      "chatid":123,
+      "date":'09 Sep 2023',
+      "name":"Charlie Jo",
+      "status":"requested"
+    },
+    {
+      "chatid":234,
+      "date":'09 Sep 2023',
+      "name":"Linh Nguyen",
+      "status":"completed"
     }
   ];
   constructor() { }
 
   ngOnInit(): void {}
+  checkStatus(status:any){
+    console.log(status)
+  }
 
 }
