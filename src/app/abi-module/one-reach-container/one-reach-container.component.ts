20f08158b8fd24b6d58b25107cd579f69a55aac0
IamGiel <lfagel84@gmail.com>
2021-10-20 16:08:23 -0400

    scroll to bottom

--- src/app/abi-module/one-reach-container/one-reach-container.component.ts
+++ src/app/abi-module/one-reach-container/one-reach-container.component.ts
@@ -8,36 +8,41 @@ import {
   SafeResourceUrl
 } from '@angular/platform-browser';
 
+
 @Component({
   selector: 'app-one-reach-container',
   templateUrl: './one-reach-container.component.html',
   styleUrls: ['./one-reach-container.component.scss']
 })
 export class OneReachContainerComponent implements OnInit, AfterViewInit {
-
   there: any = null;
   test_token = "test_token";
   url: string = 'https://chat.staging.onereach.ai/GapF3hg0QZ-V3pVTbzuZQA/abi_v3?loader=light'; // abi v2
   urlSafe!: SafeResourceUrl;
-  showLoader: boolean= true;
+  showLoader: boolean = true;
 
   constructor(
     private sanitizer: DomSanitizer,
   ) {}
 
   ngOnInit(): void {
+
     // this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(
     //   this.url + '?token=' + encodeURIComponent(this.localStorageService.retrieve('accessToken'))
     // );
-    if (!this.there) {
-      setTimeout(() => {
-        this.showLoader = false;
-      }, 10000);
-    }
+
     console.log(localStorage.getItem('message_to_onereach'));
     this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url + `?token=${this.test_token}` + `?message=${localStorage.getItem('message_to_onereach')}`)
+    // this.scrollToBottom();
   }
   ngAfterViewInit() {
-    localStorage.removeItem('message_to_onereach');
+    setTimeout(() => {
+      this.showLoader = false;
+      this.scrollToBottom()
+    }, 10000);
+  }
+
+  scrollToBottom(): void {
+    window.scrollTo(0,document.body.scrollHeight);
   }
 }
